d356efe24cd10405d7268c730eb83fc3521131f5
ivychxn <i1chan@ucsd.edu>
2021-08-12 17:33:30 -0700

    clouds b up

--- docs/src/components/nav/Navigation.js
+++ docs/src/components/nav/Navigation.js
@@ -1,6 +1,6 @@
-import './navigation.css'
-import Plane from './Plane'
-import Wave from './Wave'
+import './navigation.css';
+import Plane from './Plane';
+import Wave from './Wave';
 
 export function Navigation () {
     return (
@@ -11,6 +11,12 @@ export function Navigation () {
                 </div>
                 <div class="cloud1">
                 initiation
+                </div>
+                <div class="cloud2">
+                about
+                </div>
+                <div class="cloud3">
+                contact
                 </div>
             </div>
             <div class="wave">
