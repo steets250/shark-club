59010fc62b6ba11bcff3fea07f3a62d5355cb616
ivychxn <i1chan@ucsd.edu>
2021-08-12 18:46:43 -0700

    clouds all aligned

--- docs/src/components/nav/Navigation.js
+++ docs/src/components/nav/Navigation.js
@@ -1,27 +1,30 @@
 import './navigation.css';
 import Plane from './Plane';
 import Wave from './Wave';
+import { Link } from "react-router-dom";
 
 export function Navigation () {
     return (
         <div class="container">
-            <div class="navigation">
+            <div class="col1">
                 <div class="plane">
-                <Plane/>
+                <Plane />
                 </div>
-                <div class="cloud1">
-                initiation
-                </div>
-                <div class="cloud2">
-                about
-                </div>
-                <div class="cloud3">
-                contact
+                <div class="wave">
+                <Wave />
                 </div>
             </div>
-            <div class="wave">
-            <Wave />
-            </div>  
+            <div class="clouds">
+                <div class="cloud1">
+                <Link to="/">join us!</Link>
+                </div>
+                <div class="cloud2">
+                <Link to="/">sharkie shark</Link>
+                </div>
+                <div class="cloud3">
+                <Link to="/">contact</Link>
+                </div>
+            </div>
         </div>  
         );  
 }
